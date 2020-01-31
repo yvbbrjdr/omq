41594ee9ca033713f4a93545641ef326cb988589
yvbbrjdr <xyyqzy@hotmail.com>
2020-01-30 16:50:11 -0800

    finish config and app

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-const omq = require('./src/omq');
+const config = require('./config');
+const service = require('./src/service');
 
-// mpv --idle --fullscreen --input-ipc-server=/tmp/mpvsock
-// socat unix-connect:/tmp/mpvsock tcp-listen:12345,fork,reuseaddr
-
-(async () => {
-  const q = new omq.OCFMediaQueue();
-  q.on('stateChanged', (state) => {
-    console.log(JSON.stringify(state, null, 4));
-    if (state['now_playing'] === null)
-      q.disconnectPlayer();
-  });
-  await q.connectPlayer('127.0.0.1', 12345);
-  await q.queueMedia('jerryzhou', 'seyana', true);
-  await q.queueMedia('keur', 'test', false);
-  await q.queueMedia('jerryzhou', 'linus tech tips', true);
-  await q.queueMedia('abizer', 'departures guilty crown', false);
-  await q.queueMedia('keur', 'rickroll', false);
-  q.removeMedia('keur', 0);
-  await q.queueMedia('abizer', '前前前世', true);
-  q.skip();
-})();
+const s = new service.Service();
+s.start(config.config.wsAddr, config.config.wsPort, config.config.mpvAddr, config.config.mpvPort);
---
+++ config.js
@@ -0,0 +1,9 @@
+// mpv --idle --fullscreen --input-ipc-server=/tmp/mpvsock
+// socat unix-connect:/tmp/mpvsock tcp-listen:10678,fork,reuseaddr
+
+exports.config = {
+  wsAddr: '127.0.0.1',
+  wsPort: 10080,
+  mpvAddr: '127.0.0.1',
+  mpvPort: 10678
+}
